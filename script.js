d988694e14b3d3d73dba798dc78e099e875ed57f
leolpi <leo.lifely10@gmail.com>
2024-02-07 23:28:42 +0000

    Wrote code for sql in server.js and script.js

---
+++ script.js
@@ -0,0 +1,95 @@
+function randomLine(fileName, numLines) {
+  fetch(fileName).then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    return response.text();
+  }).then(data => {
+    const lines = data.split('\n');
+    let lastLine = -1;
+    for (let i = 0; i < numLines; i++) {
+      const randomIndex = Math.floor(Math.random() * lines.length);
+      if (randomIndex === lastLine) {
+        i--;
+        continue;
+      }
+      lastLine = randomIndex;
+      console.log('chosen line:', lines[randomIndex]);
+      document.getElementById('file-content').textContent += lines[randomIndex] + '\n';
+    }
+  }).catch(error => {
+    console.error(error);
+  });
+}
+
+var startTime;
+
+function togglePractice(on) {
+  var button = document.getElementById('start-practice-button');
+  if (on) {
+    showButtons();
+    startTime = Date.now();
+    console.log('Practice started at:', startTime);
+    button.textContent = 'End practice';
+    button.removeEventListener('click', () => togglePractice(1));
+    button.addEventListener('click', () => togglePractice(0));
+  } else {
+    hideButtons();
+    var endTime = Date.now();
+    var timeDiff = Math.round((endTime - startTime) / 60000);
+    console.log('Practice time:', timeDiff);
+    if (timeDiff > 0.5) {
+     var user = prompt('Enter your name:', 'Milo');
+     writeToDB(user, timeDiff, new Date().toISOString());
+    } else {
+      console.log('Practice time too short');
+      document.getElementById('file-content').textContent += 'Practice time too short\n';
+    }
+    button.textContent = 'Start practice';
+    button.removeEventListener('click', () => togglePractice(0));
+    button.addEventListener('click', () => togglePractice(1));
+  }
+}
+
+function hideButtons() {
+  document.getElementById('3-scales-button').style.display = 'none';
+  document.getElementById('2-book1-review-button').style.display = 'none';
+  document.getElementById('2-book2-review-button').style.display = 'none';
+  document.getElementById('2-book3-review-button').style.display = 'none';
+}
+
+function showButtons() {
+  document.getElementById('3-scales-button').style.display = 'block';
+  document.getElementById('2-book1-review-button').style.display = 'block';
+  document.getElementById('2-book2-review-button').style.display = 'block';
+  document.getElementById('2-book3-review-button').style.display = 'block';
+
+}
+
+function writeToDB(user, duration, date) {
+  fetch('/WriteDB', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user: user, duration: duration, date: date})
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.text();
+  }).then(data => {
+    console.log(data);
+  }).catch(error => {
+    console.error(error);
+  });
+}
+
+hideButtons();
+
+document.getElementById('start-practice-button').addEventListener('click', () => togglePractice(1));
+
+document.getElementById('3-scales-button').addEventListener('click', () => randomLine('scales.txt', 3));
+document.getElementById('2-book1-review-button').addEventListener('click', () => randomLine('reviews_book1.txt', 2));
+document.getElementById('2-book2-review-button').addEventListener('click', () => randomLine('reviews_book2.txt', 2));
+document.getElementById('2-book3-review-button').addEventListener('click', () => randomLine('reviews_book3.txt', 2));
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'sql-svc',
+    password: 'violin'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to MySQL.');
+});
+
+app.use(express.json());
+
+app.post('/WriteDB', (req, res) => {
+    const duration = req.body.duration;
+    const date = req.body.date;
+    const user = req.body.user;
+    var sql = `INSERT INTO practices (user, date, duration) VALUES (${user}, ${date}, ${duration})`;
+    connection.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log('1 record inserted');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
